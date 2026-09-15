1ae1891ca38b35237cd15a749fb2cb3cabeef069
hengky-cr <zerossukanasi@gmail.com>
2026-09-15 19:12:39 +0700

    versi 3

--- ifnloop.dart
+++ ifnloop.dart
@@ -32,4 +32,39 @@ void main(){
   //     print('Nilai diluar list');
   // }
   
+  //for 
+  // List datamhs=['Sindy','Windy','Karen', 'Vincent'];
+  // for (var i = 0; i < datamhs.length; i++) {
+  //   print('nama mhs ke ${i+1} adalah ${datamhs[i]}');
+  // }
+  // List datamhs=['Sindy','Windy','Karen', 'Vincent'];
+  // //for in
+  // for (var element in datamhs) {
+  //   print('nama mhs ke adalah ${element}');
+    
+  // }
+
+  //foreach
+  // List datamhs=['Sindy','Windy','Karen', 'Vincent'];
+
+  // datamhs.forEach((element){
+  //   print('nama mhs ke adalah ${element}');
+  // });
+
+  // //while 
+  // List datamhs=['Sindy','Windy','Karen', 'Vincent'];
+  // var i=0;
+  // while (i<datamhs.length) {
+  //   print('nama mhs ke ${i+1} adalah ${datamhs[i]}');
+  //   i++;
+    
+  // }
+
+  //do while
+  List datamhs=['Sindy','Windy','Karen', 'Vincent'];
+  var i=0;
+  do {
+    print('nama mhs ke ${i+1} adalah ${datamhs[i]}');
+    i++;
+  } while (i<datamhs.length);
 }
