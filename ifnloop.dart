229e1ef4bb663afadade9aa89728edbed91aea1b
hengky-cr <zerossukanasi@gmail.com>
2026-09-15 19:00:20 +0700

    versi 3

--- ifnloop.dart
+++ ifnloop.dart
@@ -1,6 +1,6 @@
 void main(){
   // if else
-  var nilai = 33;
+  //var nilai = 33;
   // if (nilai % 2 == 0){
   //   print('bilangan genap');
   // }
@@ -10,12 +10,26 @@ void main(){
   // } else {
   //   print('bilangan ganjil');
   // }
-  if (nilai % 2 ==0){
-    print('bilangan genap');
+  // if (nilai % 2 ==0){
+  //   print('bilangan genap');
 
-  }
+  // }
 
-  if (nilai % 2 !=0){
-    print('bilangan ganjil');
-  }
+  // if (nilai % 2 !=0){
+  //   print('bilangan ganjil');
+  // }
+
+  // //switch case
+  // var nilai='A';
+  // switch (nilai) {
+  //   case 'A': 
+  //     print('Nilai sangat baik');
+  //     break;
+  //   case 'B':
+  //     print('Nilai baik');
+  //     break;
+  //   default:
+  //     print('Nilai diluar list');
+  // }
+  
 }
